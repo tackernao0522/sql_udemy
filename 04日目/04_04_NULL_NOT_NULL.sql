cb330ed86dd91c16972b62b47a8b81e34b52d85e
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-15 20:27:39 +0900

    17. NULLについて利用上の注意点

---
+++ 04日目/04_04_NULL_NOT_NULL.sql
@@ -0,0 +1,26 @@
+-- IN + NULL
+SELECT * FROM customers WHERE name IS NULL;
+SELECT * FROM customers WHERE name IN ("河野 文典", "稲田 季雄") OR name IS NULL;
+
+-- NOT IN
+SELECT * FROM customers WHERE name NOT IN ("河野 文典", "稲田 季雄", NULL);
+
+-- NOT IN -> name != "河野 文典" name != "稲田 季雄" name != NULL(上記はこのようになるので全てNULLになりレコードは取り出せなくなる)
+
+-- よってレコードを取得するためには下記のようになる
+
+SELECT * FROM customers WHERE name NOT IN ("河野 文典", "稲田 季雄") AND name IS NOT NULL;
+
+-- ALL
+SELECT * FROM users WHERE birth_day <= ALL(SELECT birth_day FROM customers WHERE id < 10); # (NULLが入っているので何も取り出せない)
+
+-- customersテーブルからid<10の人の誕生日よりも古い誕生日の人をusersから取り出すSQL
+SELECT * FROM users WHERE birth_day 
+	<= ALL(SELECT birth_day 
+		FROM customers
+		WHERE id < 10 
+		AND birth_day IS NOT NULL
+		); # レコードを取り出せる
+		
+-- ALL, INの場合はNULLには気をつけましょう！
+
