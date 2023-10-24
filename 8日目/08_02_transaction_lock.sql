ae368fbbaa51f5039386873f86cd3f334a5a7464
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-24 14:34:12 +0900

    33. MySQLを用いたトランザクションロックの実演

---
+++ 8日目/08_02_transaction_lock.sql
@@ -0,0 +1,55 @@
+SHOW DATABASES;
+USE day_4_9_db;
+
+START TRANSACTION;
+
+SHOW TABLES;
+
+SELECT * FROM customers;
+
+-- 主キーでUPDATE(行ロック)
+UPDATE customers SET age=43 WHERE id=1;
+
+ROLLBACK;
+
+START TRANSACTION;
+
+-- テーブル全体のロック
+UPDATE customers SET age=42
+-- WHERE id=1;
+WHERE name="河野 文典";
+
+ROLLBACK;
+
+-- DELETE
+START TRANSACTION;
+
+-- 行ロック
+DELETE FROM customers WHERE id=1;
+
+COMMIT;
+
+-- INSERT
+START TRANSACTION;
+
+INSERT INTO customers VALUES(1, "田中 一郎", 21, '1999-10-01');
+SELECT * FROM customers;
+
+COMMIT;
+
+-- SELECTのロック
+-- FOR SHARE(共有ロック)
+-- FOR UPDATE(排他ロック)
+
+START TRANSACTION;
+
+SELECT * FROM customers WHERE id="1" FOR SHARE;
+
+ROLLBACK;
+
+START TRANSACTION;
+SELECT * FROM customers WHERE id=1 FOR UPDATE;
+ROLLBACK;
+
+
+
