3a3e6f425468a8fdb331d5738a708cfe3e78b3a7
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-24 16:23:05 +0900

    34. 明示的なテーブルロックとデッドロック

--- 02日目/Scripts/Script-13.sql
+++ 02日目/Scripts/Script-13.sql
@@ -8,3 +8,13 @@ UPDATE customers SET age=44 WHERE id=1;
 
 SELECT * FROM customers WHERE id=1 FOR SHARE;
 
+SELECT * FROM customers;
+
+UPDATE customers SET age=42 WHERE id=1; -- LOCK READがかかっていると更新できない 読み込みが可能
+
+START TRANSACTION;
+-- users -> customers
+SELECT * FROM users;
+UPDATE users SET age=12 WHERE id=1;
+
+UPDATE customers SET age=40 WHERE id=1;
--- 8日目/08_02_transaction_lock.sql
+++ 8日目/08_02_transaction_lock.sql
@@ -51,5 +51,64 @@ START TRANSACTION;
 SELECT * FROM customers WHERE id=1 FOR UPDATE;
 ROLLBACK;
 
+/*
+＃ 明示的にテーブルをロックする
+明示的にトランザクション以外にもテーブルをロックするコマンドが存在する
+*/
 
+# LOCK TABLE テーブル名 READ
+/*
+・実行したセッションは、テーブルを読み込むことができるが、書き込むことはできない
+・他のセッションも、テーブルを読み込むことはできるが、書き込むことはできない
 
+LOCK TABLE table READ
+*/
+
+LOCK TABLE customers READ;
+SELECT * FROM customers;
+UPDATE customers SET age=42 WHERE id=1; -- 更新不可
+
+# LOCKを解除する
+UNLOCK TABLES;
+
+# LOCK TABLE テーブル名 WRITE
+/*
+・実行したセッションは、テーブルを読み込むことも書き込むこともできる
+・他のセッションは、テーブルを読みことも書き込むことはできない
+
+LOCK TABLE tabe WRITE
+*/
+LOCK TABLE customers WRITE ;
+UPDATE customers SET age=42 WHERE id=1;
+
+# UNLOCK TABLES
+/*
+現セッションの保有するテーブルロックを全て解除する
+*/
+
+UNLOCK TABLES;
+
+# デッドロック
+/*
+2つのセッションが互いの更新対象のテーブルをロックしていて、処理が進まない状態のこと
+*/
+
+START TRANSACTION;
+-- customers -> users (この順で更新する 他セッションはこの逆でやってみる)
+UPDATE customers SET age=42 WHERE id=1;
+
+UPDATE users SET age=12 WHERE id=1;
+
+# アプリケーションでのトランザクション処理
+/*
+アプリケーションで、トランザクションのコードを記述することで、トランザクションを実行することができる
+
+Laravelの場合
+DB::beginTransaction();
+try {
+	$user->fill($request->all();)->save();
+	DB::commit();
+} catch (\Exception $e) {
+	DB::rollback();
+}
+*/
