3d82f17942c0478dde4b1ff24650a173a9d81078
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-29 14:43:28 +0900

    43. 副問い合わせ3 (CASEとともに使う)

--- 10日目/10_02_subquery.sql
+++ 10日目/10_02_subquery.sql
@@ -147,4 +147,26 @@ SELECT
 	WHERE 
 		cs.id < 10;
 
-	
+-- 副問い合わせ6: CASEと使う
+SELECT 
+	emp.*,
+	CASE
+	WHEN emp.department_id = (SELECT id FROM departments WHERE name="経営企画部")
+	THEN "経営層"
+	ELSE "その他"
+	END AS "役割"
+FROM
+	employees AS emp;
+
+SELECT * FROM salaries WHERE payment > (SELECT AVG(payment)  FROM salaries);
+
+SELECT
+	emp.*,
+	CASE
+		WHEN emp.id IN (
+		SELECT DISTINCT employee_id FROM salaries WHERE payment > (SELECT AVG(payment)  FROM salaries)
+		) THEN "○"
+		ELSE "x"
+		END AS "給料が平均より高いか"
+FROM
+employees emp;
