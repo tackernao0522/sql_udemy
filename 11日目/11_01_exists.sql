d77ea720fbc8592ddfa0f8c96581de82e68ee661
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-11-02 18:13:10 +0900

    46. EXISTSの基本的な使い方 2

--- 11日目/11_01_exists.sql
+++ 11日目/11_01_exists.sql
@@ -21,4 +21,17 @@ SELECT * FROM employees AS em
 		SELECT * FROM departments AS dt WHERE dt.name IN ("営業部", "開発部") AND em.department_id = dt.id
 	);
 
-	
+SELECT * FROM customers AS ct
+	WHERE EXISTS (SELECT * FROM orders AS od WHERE ct.id = od.customer_id AND od.order_date = "2020-12-31");
+
+SELECT * FROM customers AS ct
+	WHERE NOT EXISTS (SELECT * FROM orders AS od WHERE ct.id = od.customer_id AND od.order_date = "2020-12-31");
+
+SELECT * FROM employees AS em1
+	WHERE EXISTS (SELECT 1 FROM employees em2 WHERE em1.manager_id = em2.id);
+
+SELECT * FROM employees AS em1
+	WHERE EXISTS (SELECT 1 FROM employees em2 WHERE em2.manager_id = em1.id);
+
+SELECT * FROM employees AS em1
+	WHERE NOT EXISTS (SELECT 1 FROM employees em2 WHERE em2.manager_id = em1.id);
