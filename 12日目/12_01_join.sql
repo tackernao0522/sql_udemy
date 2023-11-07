193dd95da7171cee5ee8239e00f3fb602db29b4d
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-11-07 16:29:40 +0900

    52. 複雑な結合について演習

--- 12日目/12_01_join.sql
+++ 12日目/12_01_join.sql
@@ -81,3 +81,57 @@ SELECT * FROM students AS std
 		ON std.id = enr.student_id
 			RIGHT JOIN classes AS cs
 				ON enr.class_id  = cs.id;
+			
+/*
+customers, orders, items, stores を紐付ける(INNER JOIN)
+customers.idで並び替える(ORDER BY)
+*/
+SELECT
+	ct.id, ct.last_name, od.item_id, od.order_amount, od.order_price, od.order_date, it.name, st.name
+	FROM customers AS ct
+		INNER JOIN orders AS od
+			ON od.customer_id = ct.id
+				INNER JOIN items AS it
+					ON od.item_id = it.id
+						INNER JOIN stores AS st
+							ON it.store_id = st.id
+								ORDER BY ct.id;
+				
+/*
+customers, orders, items, stores を紐付ける(INNER JOIN)
+customers.idで並び替える(ORDER BY)
+customers.idが10で、orders.order_dateが 2020-08-01より後に絞り込む(WHERE)
+*/
+SELECT
+	ct.id, ct.last_name, od.item_id, od.order_amount, od.order_price, od.order_date, it.name, st.name
+	FROM customers AS ct
+		INNER JOIN orders AS od
+			ON od.customer_id = ct.id
+				INNER JOIN items AS it
+					ON od.item_id = it.id
+						INNER JOIN stores AS st
+							ON it.store_id = st.id
+								WHERE ct.id = 10 AND od.order_date > "2020-08-01"
+									ORDER BY ct.id;
+
+-- サブクエリ 上記を同じ結果になる
+SELECT
+	ct.id, ct.last_name, od.item_id, od.order_amount, od.order_price, od.order_date, it.name, st.name
+	FROM (SELECT * FROM customers WHERE id=10) AS ct
+		INNER JOIN (SELECT * FROM orders WHERE order_date > "2020-08-01") AS od
+			ON od.customer_id = ct.id
+				INNER JOIN items AS it
+					ON od.item_id = it.id
+						INNER JOIN stores AS st
+							ON it.store_id = st.id
+								ORDER BY ct.id;
+
+-- GROUP BY の紐付け
+SELECT * FROM customers AS ct
+	INNER JOIN
+		(SELECT customer_id, SUM(order_amount * order_price) AS summary_price
+			FROM orders
+				GROUP BY customer_id) AS order_summary
+					ON ct.id = order_summary.customer_id
+						ORDER BY ct.age
+							LIMIT 5;
