fdd603df4aa5d5e896a298712975482387a64dbe
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-11-07 15:19:54 +0900

    51, 外部結合(LEFT JOIN, RIGHT JOIN)

--- 12日目/12_01_join.sql
+++ 12日目/12_01_join.sql
@@ -8,7 +8,7 @@ SELECT * FROM employees; # department_idを持っている
 
 SELECT * FROM departments;
 
--- INNER JOINは下記の例で言うとdepartment_idが存在しないものは表示されない
+-- INNER JOINは下記の例で言うとdepartment_idが存在しないものは表示されない(紐付けできない部分は表示されない)
 
 -- 通常のJOIN
 SELECT * FROM employees AS emp
@@ -31,3 +31,53 @@ SELECT * FROM students AS std
 SELECT * FROM employees AS emp
 	INNER JOIN students AS std
 		ON emp.id < std.id;
+
+/*
+LEFT (OUTER) JOIN
+複数のテーブルを結合して、左のテーブルは全てのレコードを取得して、
+右のテーブルからは紐付けのできたレコードのみを取り出し、それ以外はNULLとして表示される
+*/
+SELECT emp.id, emp.first_name, emp.last_name, dt.id AS department_id, dt.name AS department_name 
+	FROM employees AS emp
+		LEFT JOIN departments AS dt
+			ON emp.department_id = dt.id;
+		
+SELECT emp.id, emp.first_name, emp.last_name, COALESCE(dt.id, "該当なし") AS department_id, dt.name AS department_name 
+	FROM employees AS emp
+		LEFT JOIN departments AS dt
+			ON emp.department_id = dt.id;
+
+# 多対多の紐付け  enrollmentsテーブルは中間テーブル
+SELECT * FROM students AS std
+	LEFT JOIN enrollments AS enr -- class_idがある
+		ON std.id = enr.student_id
+			LEFT JOIN classes AS cs
+				ON enr.class_id  = cs.id;
+
+/*
+RIGHT (OUTER) JOIN
+複数のテーブルを結合して、右のテーブルは全てのレコードを取得して、左のテーブルからは
+紐付けのできたレコードのみを取り出し、それ以外はNULLとして表示される
+*/
+SELECT * FROM students AS std
+	RIGHT JOIN enrollments AS enr -- class_idがある
+		ON std.id = enr.student_id
+			RIGHT JOIN classes AS cs
+				ON enr.class_id  = cs.id;
+
+/*
+FULL OUTER JOIN (LEFT JOINとRIGHT JOINが合わさったようなもの)
+複数のテーブルを結合して、結合できなかった行はNULLと表示する結合方法
+MySQL(ver.8.028 の時点)では、FURLL OUTER JOINを利用できないのでUNIONを利用する。ただ処理時間はかかる
+*/
+SELECT * FROM students AS std
+	LEFT JOIN enrollments AS enr -- class_idがある
+		ON std.id = enr.student_id
+			LEFT JOIN classes AS cs
+				ON enr.class_id  = cs.id
+UNION
+SELECT * FROM students AS std
+	RIGHT JOIN enrollments AS enr -- class_idがある
+		ON std.id = enr.student_id
+			RIGHT JOIN classes AS cs
+				ON enr.class_id  = cs.id;
