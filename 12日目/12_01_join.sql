f59c09ade200d4f672928faac8e1015edd54483f
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-11-07 17:17:37 +0900

    53. 交差結合 (CROSS JOIN)

--- 12日目/12_01_join.sql
+++ 12日目/12_01_join.sql
@@ -20,13 +20,13 @@ SELECT emp.id, emp.first_name, emp.last_name, dt.id AS department_id, dt.name AS
 	FROM employees AS emp
 		INNER JOIN departments AS dt
 			ON emp.department_id = dt.id;
-		
+
 -- 複数のレコードを紐付ける
 SELECT * FROM students AS std
 	INNER JOIN users AS usr
 		ON std.first_name = usr.first_name
 			AND std.last_name = usr.last_name;
-		
+
 -- =以外で紐付ける
 SELECT * FROM employees AS emp
 	INNER JOIN students AS std
@@ -41,7 +41,7 @@ SELECT emp.id, emp.first_name, emp.last_name, dt.id AS department_id, dt.name AS
 	FROM employees AS emp
 		LEFT JOIN departments AS dt
 			ON emp.department_id = dt.id;
-		
+
 SELECT emp.id, emp.first_name, emp.last_name, COALESCE(dt.id, "該当なし") AS department_id, dt.name AS department_name 
 	FROM employees AS emp
 		LEFT JOIN departments AS dt
@@ -81,7 +81,7 @@ SELECT * FROM students AS std
 		ON std.id = enr.student_id
 			RIGHT JOIN classes AS cs
 				ON enr.class_id  = cs.id;
-			
+
 /*
 customers, orders, items, stores を紐付ける(INNER JOIN)
 customers.idで並び替える(ORDER BY)
@@ -96,7 +96,7 @@ SELECT
 						INNER JOIN stores AS st
 							ON it.store_id = st.id
 								ORDER BY ct.id;
-				
+
 /*
 customers, orders, items, stores を紐付ける(INNER JOIN)
 customers.idで並び替える(ORDER BY)
@@ -135,3 +135,111 @@ SELECT * FROM customers AS ct
 					ON ct.id = order_summary.customer_id
 						ORDER BY ct.age
 							LIMIT 5;
+
+/*
+SELF JOIN 自己結合 同一のテーブルを結合する結合方法
+同じテーブルで結合するので別のテーブル名をつけていく
+*/
+SELECT CONCAT(emp1.last_name, emp1.first_name) AS "部下の名前",
+			emp1.age AS "部下の年齢",
+				CONCAT(emp2.last_name, emp2.first_name) AS "上司の名前",
+					emp2.age AS "上司の年齢"
+						FROM employees AS emp1
+							INNER JOIN employees AS emp2
+								ON emp1.manager_id = emp2.id;
+
+SELECT CONCAT(emp1.last_name, emp1.first_name) AS "部下の名前",
+			emp1.age AS "部下の年齢",
+				CONCAT(emp2.last_name, emp2.first_name) AS "上司の名前",
+					emp2.age AS "上司の年齢"
+						FROM employees AS emp1
+							LEFT JOIN employees AS emp2
+								ON emp1.manager_id = emp2.id;
+
+SELECT CONCAT(emp1.last_name, emp1.first_name) AS "部下の名前",
+			emp1.age AS "部下の年齢",
+				COALESCE(CONCAT(emp2.last_name, emp2.first_name), "該当なし") AS "上司の名前",
+					emp2.age AS "上司の年齢"
+						FROM employees AS emp1
+							LEFT JOIN employees AS emp2
+								ON emp1.manager_id = emp2.id;
+
+/*
+交差結合(CROSS JOIN)
+2つのテーブルのデータの全ての組み合わせを取得するSQL
+```
+SELECT (カラム) FROM テーブル1 AS 別名1
+CROSS JOIN テーブル2 AS 別名2
+(ON 結合条件)
+```
+
+古い結合の書き方
+```
+SELECT (カラム)
+FROM テーブル1 AS 別名1, テーブル2 AS 別名2
+(WHERE 条件)
+```
+*/
+SELECT * FROM employees AS emp1, employees AS emp2;
+
+SELECT * FROM employees AS emp1, employees AS emp2
+	WHERE emp1.id = 1;
+
+SELECT * FROM employees AS emp1 CROSS JOIN employees AS emp2; -- 別の書き方
+
+SELECT * FROM employees AS emp1 
+	CROSS JOIN employees AS emp2
+		ON emp1.id < emp2.id;
+
+/*
+計算結果とCASEで紐付け
+*/
+SELECT
+*
+	FROM customers AS cs
+		CROSS JOIN(
+			SELECT AVG(age) AS avg_age FROM customers
+				) AS summary_customers;
+
+SELECT
+*,
+CASE
+	WHEN cs.age > summary_customers.avg_age THEN "○"
+	ELSE "x"
+END AS "平均年齢よりも年齢が高いか"
+	FROM customers AS cs
+		CROSS JOIN(
+			SELECT AVG(age) AS avg_age FROM customers
+				) AS summary_customers;
+
+SELECT
+*
+FROM employees AS emp
+	INNER JOIN salaries AS sa
+		ON emp.id = sa.employee_id
+			CROSS JOIN
+				(SELECT AVG(payment) AS avg_payment FROM salaries) AS summary;
+
+SELECT
+emp.id, AVG(payment)
+FROM employees AS emp
+	INNER JOIN salaries AS sa
+		ON emp.id = sa.employee_id
+			CROSS JOIN
+				(SELECT AVG(payment) AS avg_payment FROM salaries) AS summary
+					GROUP BY emp.id;
+
+SELECT
+emp.id,
+AVG(payment),
+summary.avg_payment,
+CASE
+	WHEN AVG(payment) >= summary.avg_payment THEN "○"
+	ELSE "x"
+END AS "平均月収以上か"
+FROM employees AS emp
+	INNER JOIN salaries AS sa
+		ON emp.id = sa.employee_id
+			CROSS JOIN
+				(SELECT AVG(payment) AS avg_payment FROM salaries) AS summary
+					GROUP BY emp.id, summary.avg_payment;
