bd1027eddb9865f451400dcf75559421d93655dc
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-11-21 19:58:37 +0900

    74. 制約の追加(DEFAULT, CHECK, NOT NULL, 主キー, 外部キー)

--- 16日目/16_02_constraint.sql
+++ 16日目/16_02_constraint.sql
@@ -33,4 +33,50 @@ SELECT * FROM employees;
 -- INSERT INTO employees VALUES(2, "00000003", "Taro", 19); エラーになる
 
 -- CREATE文を確認
-SHOW CREATE TABLE employees;
+
+-- DEFAULT追加
+SELECT * FROM customers;
+
+SHOW CREATE TABLE customers;
+
+-- CHECK制約の削除
+ALTER TABLE customers DROP CONSTRAINT customers_chk_1;
+
+DESCRIBE customers;
+
+ALTER TABLE customers
+ALTER age SET DEFAULT 20;
+
+INSERT INTO customers(id, name) VALUES(2, "Jiro");
+
+SELECT * FROM customers;
+
+-- NOT NULLの追加
+ALTER TABLE customers MODIFY name VARCHAR(255) NOT NULL;
+
+INSERT INTO customers(id, name) VALUES(3, NULL); -- エラーになる
+
+-- CHECK制約の追加
+ALTER TABLE customers ADD CONSTRAINT check_age CHECK(age > 20); -- 既に20以下のレコードが入っているのでエラーになる
+
+ALTER TABLE customers ADD CONSTRAINT check_age CHECK(age >= 20); 
+
+DESCRIBE customers;
+
+-- 主キーの削除
+ALTER TABLE customers DROP PRIMARY KEY;
+
+-- 主キーの追加
+ALTER TABLE customers
+ADD CONSTRAINT pk_customers PRIMARY KEY(id);
+
+-- 外部キー
+DESCRIBE students;
+
+SHOW CREATE TABLE students;
+
+ALTER TABLE students DROP CONSTRAINT students_ibfk_1;
+
+ALTER TABLE students
+ADD CONSTRAINT fk_schools_students
+FOREIGN KEY(school_id) REFERENCES schools(id);
