03e92b99a9d5da17570f5eee806d78e0fd350e7c
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-12-11 14:14:49 +0900

    95. インデックスが利用できないパターン2

--- 20日目/20_02_not_used_index.sql
+++ 20日目/20_02_not_used_index.sql
@@ -39,3 +39,62 @@ EXPLAIN ANALYZE SELECT * FROM customers WHERE age+2=27; -- FULL SCANになって
 -> Filter: ((customers.age + 2) = 27)  (cost=50524 rows=497786) (actual time=0.107..510 rows=10286 loops=1)
     -> Table scan on customers  (cost=50524 rows=497786) (actual time=0.0832..455 rows=500000 loops=1)
 */
+
+-- 文字列と数値の比較(INDEXは使えない FULL SCANになる)
+CREATE INDEX idx_customers_prefecture_code ON customers(prefecture_code);
+EXPLAIN ANALYZE SELECT * FROM customers WHERE prefecture_code=21;
+/*
+-> Filter: (customers.prefecture_code = 21)  (cost=50524 rows=49779) (actual time=0.145..519 rows=12192 loops=1)
+    -> Table scan on customers  (cost=50524 rows=497786) (actual time=0.133..446 rows=500000 loops=1)
+*/
+DESCRIBE customers; -- prefecture_codeはchar型(文字列)である
+
+EXPLAIN ANALYZE SELECT * FROM customers WHERE prefecture_code="21";
+/*
+ -> Index lookup on customers using idx_customers_prefecture_code (prefecture_code='21'), with index condition: (customers.prefecture_code = '21')  (cost=4429 rows=21942) (actual time=1.02..285 rows=12192 loops=1)
+*/
+
+-- 前方一致、中間一致、後方一致 前方一致はINDEXが使われる それ以外は使用されない
+CREATE INDEX idx_customers_first_name ON customers(first_name);
+EXPLAIN ANALYZE SELECT * FROM customers WHERE first_name LIKE 'Jo%';
+/*
+-> Index range scan on customers using idx_customers_first_name over ('Jo' <= first_name <= 'Jo􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿􏿿'), with index condition: (customers.first_name like 'Jo%')  (cost=23498 rows=52218) (actual time=0.0856..287 rows=24521 loops=1)
+*/
+
+-- 後方一致はフルスキャンになる
+EXPLAIN ANALYZE SELECT * FROM customers WHERE first_name LIKE '%A';
+/*
+-> Filter: (customers.first_name like '%A')  (cost=50524 rows=55304) (actual time=0.108..594 rows=92504 loops=1)
+    -> Table scan on customers  (cost=50524 rows=497786) (actual time=0.105..463 rows=500000 loops=1)
+*/
+
+-- 中間一致はフルスキャン
+EXPLAIN ANALYZE SELECT * FROM customers WHERE first_name LIKE "%Jo%";
+/*
+-> Filter: (customers.first_name like '%Jo%')  (cost=50524 rows=55304) (actual time=0.124..563 rows=24521 loops=1)
+    -> Table scan on customers  (cost=50524 rows=497786) (actual time=0.117..451 rows=500000 loops=1)
+*/
+
+-- 考えられる対策として
+EXPLAIN ANALYZE SELECT * FROM customers WHERE first_name LIKE "%Jo%" LIMIT 50000; -- first_nameにJoを含む人の最初に50000件
+/*
+-> Limit: 50000 row(s)  (cost=50524 rows=50000) (actual time=2.64..590 rows=24521 loops=1)
+    -> Filter: (customers.first_name like '%Jo%')  (cost=50524 rows=55304) (actual time=2.64..588 rows=24521 loops=1)
+        -> Table scan on customers  (cost=50524 rows=497786) (actual time=2.63..467 rows=500000 loops=1)
+*/
+
+-- customersから50000件取ってきたうち、first_nameにJoを含む人
+EXPLAIN ANALYZE SELECT * FROM (SELECT * FROM customers LIMIT 50000) AS tmp WHERE first_name LIKE "%Jo%"; -- 副問い合わせにする
+/*
+-> Filter: (tmp.first_name like '%Jo%')  (cost=55515..5628 rows=5555) (actual time=144..169 rows=2423 loops=1)
+    -> Table scan on tmp  (cost=55524..56151 rows=50000) (actual time=144..158 rows=50000 loops=1)
+        -> Materialize  (cost=55524..55524 rows=50000) (actual time=144..144 rows=50000 loops=1)
+            -> Limit: 50000 row(s)  (cost=50524 rows=50000) (actual time=0.0894..70.4 rows=50000 loops=1)
+                -> Table scan on customers  (cost=50524 rows=497786) (actual time=0.0887..64.5 rows=50000 loops=1)
+*/
+
+SHOW INDEX FROM customers;
+
+DROP INDEX idx_customers_age ON customers;
+DROP INDEX idx_customers_prefecture_code ON customers;
+DROP INDEX idx_customers_first_name ON customers;
