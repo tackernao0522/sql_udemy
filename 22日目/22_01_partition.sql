f07b679e274fa5277bb67e14f62cc4ce839eaad7
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-12-20 18:17:09 +0900

    105 リストパーティション

--- 22日目/22_01_partition.sql
+++ 22日目/22_01_partition.sql
@@ -50,7 +50,7 @@ CREATE TABLE `sales_history_partitioned` (
   `sales_day` date NOT NULL DEFAULT '1970-01-01',
   PRIMARY KEY (`id`,`sales_day`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2500001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-/*!50100 PARTITION BY RANGE (year(`sales_day`))
+!50100 PARTITION BY RANGE (year(`sales_day`))
 (PARTITION p0_lt_2016 VALUES LESS THAN (2016) ENGINE = InnoDB,
  PARTITION p1_lt_2017 VALUES LESS THAN (2017) ENGINE = InnoDB,
  PARTITION p2_lt_2018 VALUES LESS THAN (2018) ENGINE = InnoDB,
@@ -58,7 +58,7 @@ CREATE TABLE `sales_history_partitioned` (
  PARTITION p4_lt_2020 VALUES LESS THAN (2020) ENGINE = InnoDB,
  PARTITION p5_lt_2021 VALUES LESS THAN (2021) ENGINE = InnoDB,
  PARTITION p6_lt_max VALUES LESS THAN MAXVALUE ENGINE = InnoDB)
- */
+*/
 
 SELECT COUNT(*) FROM sales_history_partitioned; -- パーティション化されている
 SELECT COUNT(*) FROM sales_history; -- パーティション化されていない
@@ -71,3 +71,37 @@ WHERE sales_day BETWEEN "2016-01-01" AND "2016-12-31"; -- 358ms
 
 EXPLAIN SELECT COUNT(*) FROM sales_history_partitioned
 WHERE sales_day BETWEEN "2016-01-01" AND "2016-12-31";
+
+-- リストパーティション
+
+CREATE TABLE shops(
+    id INT,
+    name VARCHAR(50),
+    shop_type INT
+)
+PARTITION BY LIST(shop_type)(
+    PARTITION p0 VALUES IN(1,2,3),
+    PARTITION p1 VALUES IN(4,5),
+    PARTITION p2 VALUES IN(6,7)
+);
+
+INSERT INTO shops VALUES
+(1, "Shop A", 1),
+(2, "Shop B", 2),
+(3, "Shop C", 3),
+(4, "Shop D", 4),
+(5, "Shop E", 5),
+(6, "Shop F", 6),
+(7, "Shop G", 7);
+
+SELECT * FROM shops PARTITION(p0);
+SELECT * FROM shops PARTITION(p0, p1, p2);
+
+-- パーティション追加
+ALTER TABLE shops ADD PARTITION
+(PARTITION p3 VALUES IN(8,9,10));
+
+INSERT INTO shops VALUES(8, "SHOP H", 8);
+SELECT * FROM shops PARTITION(p3);
+
+INSERT INTO shops VALUES(9, "SHOP H", 21); -- エラーになる
