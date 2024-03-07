ef707ee0bc247f287e9162661c748910f4174a17
tackernao0522 <53789796+tackernao0522@users.noreply.github.com>
2024-03-07 16:05:09 +0900

    Update 04_04_NULL_NOT_NULL.sql

--- 04日目/04_04_NULL_NOT_NULL.sql
+++ 04日目/04_04_NULL_NOT_NULL.sql
@@ -1,6 +1,6 @@
 -- 4日目のDocumentのnullの評価式を確認する
 -- IN + NULL
-SELECT * FROM customers WHERE name IS NULL;
+SELECT * FROM customers WHERE name IS NULL; # NULLの人を取得する
 SELECT * FROM customers WHERE name IN ("河野 文典", "稲田 季雄") OR name IS NULL;
 
 -- NOT IN
